ede57a3e43656b88f31ff46b322b5ac39634adf0
NeimadTL <damientalbot26@gmail.com>
2026-01-17 19:55:10 -0400

    Add second iteration
    This improves a bit on first iteration but not quite there yet.
    Still a bit buggy.

--- number_to_human.rb
+++ number_to_human.rb
@@ -1,26 +1,18 @@
 PRECISION = 3
 UNITS_TABLE = {
  3 => "Thousand",
- 6 => "Million",
- 9 => ""
+ 6  => "Million",
+ 9 => "Billion"
 }
 
-def number_to_human(number, opts = {})  
-  case (digits(number))
-  when 4..6
-    return "#{number/1000.0} Thousand"
-  when 7..9
-    return "#{number/1000000.0} Million"
-  when 10..12
-    return "#{number/1000000000.0} Billion"
-  else
-    number.to_s
-  end
+def number_to_human(number, opts = {})
+  exponent = exponent(number)
+  return "#{number/10.0**exponent} #{UNITS_TABLE[exponent]}".strip  
 end
 
 
-def digits(number)
-  Math.log10(number).to_i + 1
+def exponent(number)
+  Math.log10(number).floor
 end
 
 
@@ -28,6 +20,8 @@ puts "=> #{number_to_human(1).inspect}"
 puts "=> #{number_to_human(10).inspect}"
 puts "=> #{number_to_human(100).inspect}"
 puts "=> #{number_to_human(1000).inspect}" # Thousand
+puts "=> #{number_to_human(1234).inspect}" # Thousand
+puts "=> #{number_to_human(12345).inspect}" # Thousand
 puts "=> #{number_to_human(1000000).inspect}" # Million
 puts "=> #{number_to_human(1000000000).inspect}" # Billion
 
