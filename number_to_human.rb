95a6b752e7a9ccadf2a246aa24c82c89e371b2a5
NeimadTL <damientalbot26@gmail.com>
2026-01-19 09:02:25 -0400

    Improve second iteration
    This improves the second iteration by adjusting the exponent.
    If the exponent is divisible by 3, all is fine to pick a unit
    present in the `UNITS_TABLE`. If not, the exponent has to be
    adjusted to be able to pick an unit in the table.

--- number_to_human.rb
+++ number_to_human.rb
@@ -1,27 +1,39 @@
 PRECISION = 3
 UNITS_TABLE = {
  3 => "Thousand",
- 6  => "Million",
+ 6 => "Million",
  9 => "Billion"
 }
 
 def number_to_human(number, opts = {})
   exponent = exponent(number)
-  return "#{number/10.0**exponent} #{UNITS_TABLE[exponent]}".strip  
+
+  "#{number/10.0**exponent} #{UNITS_TABLE[exponent]}".strip  
 end
 
-
 def exponent(number)
-  Math.log10(number).floor
+  exponent = Math.log10(number).floor
+
+  unless exponent.modulo(3).zero?
+    exponent = 3 if (3..5).include?(exponent)
+    exponent = 6 if (6..8).include?(exponent)
+    exponent = 9 if (9..12).include?(exponent)
+  end
+
+  exponent
 end
 
 
 puts "=> #{number_to_human(1).inspect}"
-puts "=> #{number_to_human(10).inspect}"
-puts "=> #{number_to_human(100).inspect}"
-puts "=> #{number_to_human(1000).inspect}" # Thousand
-puts "=> #{number_to_human(1234).inspect}" # Thousand
-puts "=> #{number_to_human(12345).inspect}" # Thousand
-puts "=> #{number_to_human(1000000).inspect}" # Million
-puts "=> #{number_to_human(1000000000).inspect}" # Billion
+puts "=> #{number_to_human(12).inspect}"
+puts "=> #{number_to_human(123).inspect}"
+puts "=> #{number_to_human(1234).inspect}"
+puts "=> #{number_to_human(12345).inspect}"
+puts "=> #{number_to_human(123456).inspect}"
+puts "=> #{number_to_human(1234567).inspect}"
+puts "=> #{number_to_human(12345678).inspect}"
+puts "=> #{number_to_human(123456789).inspect}"
+puts "=> #{number_to_human(1234567891).inspect}"
+puts "=> #{number_to_human(12345678912).inspect}"
+puts "=> #{number_to_human(123456789123).inspect}"
 
