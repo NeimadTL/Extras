fdfb78481ff0f32adef7212e9717236ff1c6bd70
NeimadTL <damientalbot26@gmail.com>
2026-01-26 14:57:10 -0400

    Make it possible set units
    Though it's a bit drafty,  one can set its own units when calling
    the method. It's drafty for those reasons:
    1) Once it gets called one time, the default units are replaced by
       the ones provided even if the units paramater is not passed when
       making another call to the method.
    2) Since one has to remember the units, it'd be better to have
       meaningful names instead of numbers.

--- number_to_human.rb
+++ number_to_human.rb
@@ -36,10 +36,12 @@ def human_friendly_number(number, exponent, opts)
   precision = opts[:precision] ? opts[:precision] : PRECISION
   strip_zeros = opts[:strip_insignificant_zeros]
   separator = opts[:separator]
+  units = opts[:units]
 
   value = strip_unsignificant_zeros(calculate_value(number, exponent, precision))
   value = calculate_value(number, exponent, precision) if strip_zeros == false
   value = value.to_s.gsub('.', separator) unless separator.nil?
+  UNITS_TABLE.update(units) unless units.nil?
   unit = determine_unit(exponent)
 
   "#{value} #{unit}".strip
@@ -54,7 +56,7 @@ end
 puts "=> #{number_to_human(1).inspect}"
 puts "=> #{number_to_human(12).inspect}"
 puts "=> #{number_to_human(123).inspect}"
-puts "=> #{number_to_human(1234).inspect}"
+puts "=> #{number_to_human(1234, units: {3=>'K', 9=>'B'}).inspect}"
 puts "=> #{number_to_human(12_345).inspect}"
 puts "=> #{number_to_human(123_456).inspect}"
 puts "=> #{number_to_human(1_234_567).inspect}"
