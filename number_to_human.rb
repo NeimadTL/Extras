7a1e1406451edf3b687f72bdea3445e5925fba5d
NeimadTL <damientalbot26@gmail.com>
2026-02-02 17:21:11 -0400

    Make object out of existing code
    An object has been made out of the existing code so that each
    instance has its own set of attributes and therefore preventing
    unwanted custom units override.

--- number_to_human.rb
+++ number_to_human.rb
@@ -1,85 +1,90 @@
 #integers: :unit, :ten, :hundred, :thousand, :million, :billion, :trillion, :quadrillion
 #fractionals: :deci, :centi, :mili, :micro, :nano, :pico, :femto
 
+class NumberToHumanConverter
 
-class InvalidNumberError < StandardError; end
+  PRECISION = 3
+  UNIT_TABLE = {
+   :thousand => 3,
+   :million => 6,
+   :billion => 9
+  }
 
-PRECISION = 3
-UNITS_TABLE = {
- :thousand => 3,
- :million => 6,
- :billion => 9
-}
-EXPONENTS_TABLE = UNITS_TABLE.invert
-
-def number_to_human(number, opts = {})
-  raise 'Number must be Integer or Float' unless [Integer, Float].include?(number.class)
-
-  exponent = exponent(number)
-  human_friendly_number(number, exponent, opts)
-end
-
-def exponent(number)
-  exponent = Math.log10(number).floor
-
-  unless exponent.modulo(3).zero?
-    exponent = 0 if exponent < 3
-    exponent = 3 if (3..5).include?(exponent)
-    exponent = 6 if (6..8).include?(exponent)
-    exponent = 9 if (9..12).include?(exponent)
+  def initialize(number, opts = {})
+    @number = number
+    @opts = opts
+    @exponent_table = UNIT_TABLE.invert
   end
 
-  exponent
-end
+  def convert
+    raise 'Number must be Integer or Float' unless [Integer, Float].include?(@number.class)
 
-def calculate_value(number, exponent, precision)
-  (number/10.0**exponent).truncate(precision)
-end
+    exponent = determine_exponent
+    human_friendly_number(exponent)
+  end
 
-def determine_unit(exponent)
-  EXPONENTS_TABLE[exponent]
-end
+  def determine_exponent
+    exponent = Math.log10(@number).floor
 
-def human_friendly_number(number, exponent, opts)
-  precision = opts[:precision] || PRECISION
-  strip_zeros = opts[:strip_insignificant_zeros]
-  separator = opts[:separator]
-  units = opts[:units]
+    unless exponent.modulo(3).zero?
+      exponent = 0 if exponent < 3
+      exponent = 3 if (3..5).include?(exponent)
+      exponent = 6 if (6..8).include?(exponent)
+      exponent = 9 if (9..12).include?(exponent)
+    end
 
-  value = strip_unsignificant_zeros(calculate_value(number, exponent, precision))
-  value = calculate_value(number, exponent, precision) if strip_zeros == false
-  value = value.to_s.gsub('.', separator) unless separator.nil?
-  update_units(units) unless units.nil?
-  unit = determine_unit(exponent)
+    exponent
+  end
 
-  "#{value} #{unit}".strip
-end
+  def calculate_value(exponent, precision)
+    (@number/10.0**exponent).truncate(precision)
+  end
 
-def strip_unsignificant_zeros(value)
-  float, integer = value, value.to_i
+  def determine_unit(exponent)
+    @exponent_table[exponent]
+  end
 
-  float == integer ? integer : float
-end
+  def human_friendly_number(exponent)
+    precision = @opts[:precision] || PRECISION
+    strip_zeros = @opts[:strip_insignificant_zeros]
+    separator = @opts[:separator]
+    units = @opts[:units]
 
-def update_units(custom_units)
-  custom_units.each do |unit_key, custom_v|
-    lookup_key = EXPONENTS_TABLE.key(unit_key)
-    EXPONENTS_TABLE[lookup_key] = custom_v
+    value = strip_unsignificant_zeros(calculate_value(exponent, precision))
+    value = calculate_value(exponent, precision) if strip_zeros == false
+    value = value.to_s.gsub('.', separator) unless separator.nil?
+    update_units(units) unless units.nil?
+    unit = determine_unit(exponent)
+
+    "#{value} #{unit}".strip
+  end
+
+  def strip_unsignificant_zeros(value)
+    float, integer = value, value.to_i
+
+    float == integer ? integer : float
+  end
+
+  def update_units(custom_units)
+    custom_units.each do |unit_key, custom_v|
+      lookup_key = @exponent_table.key(unit_key)
+      @exponent_table[lookup_key] = custom_v
+    end
   end
 end
 
-puts "=> #{number_to_human(1).inspect}"
-puts "=> #{number_to_human(12).inspect}"
-puts "=> #{number_to_human(123).inspect}"
-puts "=> #{number_to_human(1234, units: {:thousand=>'K', :billion=>'B'}).inspect}"
-puts "=> #{number_to_human(12_345).inspect}"
-puts "=> #{number_to_human(123_456).inspect}"
-puts "=> #{number_to_human(1_234_567).inspect}"
-puts "=> #{number_to_human(12_345_678).inspect}"
-puts "=> #{number_to_human(123_456_789).inspect}"
-puts "=> #{number_to_human(1_234_567_890).inspect}"
-puts "=> #{number_to_human(12_345_678_901).inspect}"
-puts "=> #{number_to_human(123_456_789_012).inspect}"
-puts "-> #{number_to_human(9.00001).inspect}"
-puts "-> #{number_to_human(9.00001, strip_insignificant_zeros: false, precision: 5, separator: ',').inspect}"
-puts "=> #{number_to_human('a').inspect}"
+puts "=> #{NumberToHumanConverter.new(1).convert.inspect}"
+puts "=> #{NumberToHumanConverter.new(12).convert.inspect}"
+puts "=> #{NumberToHumanConverter.new(123).convert.inspect}"
+puts "=> #{NumberToHumanConverter.new(1234, units: {:thousand=>'K', :billion=>'B'}).convert.inspect}"
+puts "=> #{NumberToHumanConverter.new(12_345).convert.inspect}"
+puts "=> #{NumberToHumanConverter.new(123_456).convert.inspect}"
+puts "=> #{NumberToHumanConverter.new(1_234_567).convert.inspect}"
+puts "=> #{NumberToHumanConverter.new(12_345_678).convert.inspect}"
+puts "=> #{NumberToHumanConverter.new(123_456_789).convert.inspect}"
+puts "=> #{NumberToHumanConverter.new(1_234_567_890).convert.inspect}"
+puts "=> #{NumberToHumanConverter.new(12_345_678_901).convert.inspect}"
+puts "=> #{NumberToHumanConverter.new(123_456_789_012).convert.inspect}"
+puts "-> #{NumberToHumanConverter.new(9.00001).convert.inspect}"
+puts "-> #{NumberToHumanConverter.new(9.00001, strip_insignificant_zeros: false, precision: 5, separator: ',').convert.inspect}"
+puts "=> #{NumberToHumanConverter.new('a').convert.inspect}"
