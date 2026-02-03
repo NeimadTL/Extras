9dccff73bdcca77a144c931daa1d073befb02d49
NeimadTL <damientalbot26@gmail.com>
2026-02-03 18:43:29 -0400

    Add Kotlin version of number_to_human
    This is roughly how the code would look like for making a number
    more human friendly in Kotlin.

---
+++ number_to_human.kt
@@ -0,0 +1,19 @@
+import kotlin.math.*
+
+fun main() {
+    val number = 0.001
+    val unitsMap = mapOf("Nano" to -9, "Micro" to -6, "Milli" to -3 , "Unit" to 0, "Thousand" to 3, "Million" to 6, "Billion" to 9)
+    var exponent = Math.floor(Math.log10(number)).toInt()
+    if (!exponent.mod(3).equals(0)){
+        if ((-9..-7).contains(exponent)) exponent = -9
+        if ((-6..-4).contains(exponent)) exponent = -6
+        if ((-3..-1).contains(exponent)) exponent = -3
+        if ((0..2).contains(exponent)) exponent = 0
+        if ((3..5).contains(exponent)) exponent = 3
+        if ((6..0).contains(exponent)) exponent = 6
+        if ((9..12).contains(exponent)) exponent = 9
+    }
+    val unit =  unitsMap.entries.associate{(k,v)-> v to k}.get(exponent)
+    val value = (number/10.0.pow(exponent))
+    println("$value $unit")
+}
