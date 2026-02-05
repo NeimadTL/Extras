8e68115bf30940dc039c52d48054ce4c9d14b566
NeimadTL <damientalbot26@gmail.com>
2026-02-04 21:08:23 -0400

    Add custom units handling
    This part handle custom units with Ampere units as example.

--- number_to_human.kt
+++ number_to_human.kt
@@ -2,6 +2,7 @@ import kotlin.math.*
 
 fun main() {
     val number = 0.001
+    var customUnit = mapOf("Nano" to "nA", "Micro" to "μA", "Milli" to "mA" , "Unit" to "A", "Thousand" to "kA", "Million" to "MA", "Billion" to "GA")
     val unitsMap = mapOf("Nano" to -9, "Micro" to -6, "Milli" to -3 , "Unit" to 0, "Thousand" to 3, "Million" to 6, "Billion" to 9)
     var exponent = Math.floor(Math.log10(number)).toInt()
     if (!exponent.mod(3).equals(0)){
@@ -13,7 +14,13 @@ fun main() {
         if ((6..0).contains(exponent)) exponent = 6
         if ((9..12).contains(exponent)) exponent = 9
     }
-    val unit =  unitsMap.entries.associate{(k,v)-> v to k}.get(exponent)
+    var exponentsMap = unitsMap.entries.associate { (k, v) -> v to k }.toMutableMap()
+    customUnit.forEach { (unit_key, custom_v) ->
+        val lookup_key = exponentsMap.filterValues { it == unit_key }.keys;
+	if (lookup_key.iterator().hasNext()) exponentsMap.put(lookup_key.iterator().next(), custom_v)
+    }
+
+    val unit = exponentsMap.get(exponent)
     val value = (number/10.0.pow(exponent))
     println("$value $unit")
 }
